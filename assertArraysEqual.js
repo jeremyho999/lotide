0e55e61e9e0d3bffbf8de5058ee28166ad0f537e
Jeremy Ho <jeremyho999@gmail.com>
2022-11-20 21:44:11 -0700

    Add assertArraysEqual.js which assert two arrays are equal or not

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+/*
+- Take in 2 arrays and log an appropriate message to the console.
+*/
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("✅✅✅Assertion Passed:", arr1, "===", arr2);
+  } else {
+    console.log("🛑🛑🛑Assertion Failed:", arr1, "!==", arr2);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
