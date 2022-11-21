b4f09ed70f6da74bd907dfd3ed2458b3904f6af6
Jeremy Ho <jeremyho999@gmail.com>
2022-11-20 23:59:05 -0700

    Add without.js which removes unwanted items from an array

---
+++ without.js
@@ -0,0 +1,54 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("✅✅✅Assertion Passed:", arr1, "===", arr2);
+  } else {
+    console.log("🛑🛑🛑Assertion Failed:", arr1, "!==", arr2);
+  }
+};
+
+/*
+- Take in a source array and a "itemsToRemove" array and return a new array with only those items from the source array
+  that are not present in the other array.
+*/
+
+const without = function(source, itemsToRemove) {
+  //let output = source;    //I don't know why this line will change the original source array. I have to try
+  // the following 4 lines. And they work. Weird!
+  let output = [];
+  for (let index = 0; index < source.length; index++) {
+    output.push(source[index]);
+  }
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        output.splice(i, 1);
+      }
+    }
+  }
+  return output;
+};
+
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+//console.log(without(words, ["lighthouse"]));
+//console.log(words);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
