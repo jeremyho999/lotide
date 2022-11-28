eaf0ae1ffa175e14007b11f16501e2cb8203b10c
Jeremy Ho <jeremyho999@gmail.com>
2022-11-27 21:07:42 -0700

    Add M1W2_takeUntil which is similar to slice but is a HOF

--- map.js
+++ map.js
@@ -23,7 +23,7 @@ const map = function(array, callback) {
   //console.log("array: ", array);
   //console.log("callback: ", callback);
   const results = [];
-  for (let item of array) {
+  for (const item of array) {
     //console.log("item BEFORE: ", item);
     //console.log("item AFTER: ", callback(item));
     //console.log("---");
