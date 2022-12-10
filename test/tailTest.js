2e5e080dcf01a755eaa7d757dfab5ece195ac735
Jeremy Ho <jeremyho999@gmail.com>
2022-12-09 20:38:06 -0700

    Refactor tail & middle with Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,29 @@
-const assertEqual = require("../assertEqual");
+//const assertEqual = require("../assertEqual");
+//const tail = require("../tail");
+
+
+const assert = require("chai").assert;
 const tail = require("../tail");
 
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(['Hello', 'Lighthouse', 'Labs']);
+    assert.deepEqual(result, ['Lighthouse', 'Labs']);
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+  it("does not modify the input array", () => {
+    const words = ["Hello", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(words, ["Hello", "Lighthouse", "Labs"]);
+  });
 
-const words = ["Hello", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it("returns [] for an empty input array", () => {
+    const tailedArr = tail([]);
+    assert.deepEqual(tailedArr, []);
+  });
 
-const tailedArr = tail([]);
-assertEqual(tailedArr.length, 0);
-
-const tailedResult = tail([1]);
-assertEqual(tailedResult.length, 0);
+  it("returns [] for a single-element input array", () => {
+    const tailedResult = tail([1]);
+    assert.deepEqual(tailedResult, []);
+  });
+});
